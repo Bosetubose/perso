ef46588ad6a83f224497575f3cd827f2d4c18b39
Bosetubose <guillaume.dblms@gmail.com>
2016-08-17 22:47:11 +0200

    Update ft_split_whitespaces.c

--- d07/ex04/ft_split_whitespaces.c
+++ d07/ex04/ft_split_whitespaces.c
@@ -1,54 +1,69 @@
-#define	condition_de_separation 
+#define	checkspace_forward
 {
-	((src[i] == ' ') || (src[i] == 9) || (src[i] == '\n'))
-	&& ((src[i + 1] != ' ') || (src[i + 1] != 9) || (src[i + 1] != '\n'))
+	(((src[i] == ' ') || (src[i] == 9) || (src[i] == '\n'))
+	&& ((src[i + 1] != ' ') || (src[i + 1] != 9) || (src[i + 1] != '\n')))
 }
 
-int		ct_words(char *src)
+#define checkspace_backward
 {
-	int word_ct;
+	(((src[i] == ' ') || (src[i] == 9) || (src[i] == '\n'))	
+	&& ((src[i - 1] != ' ') || (src[i - 1] != 9) || (src[i - 1] != '\n')))
+}
+
+int	counter(char *src)
+{
+	int w;
 	int i;
 	
 	i = 0;
-	word_ct = 1;
+	w = 1;
 	while (src[i])
 	{
-  		if condition_de_separation
-  			word_ct++;
+  		if checkspace_forward
+  			w++;
   		i++;
   	}
+  	return (w);
 }
-
-char	**store(char **src, int size, int word)
+void	writer(char **srr, int i, int j, int w)
 {
-	src[word] = (char*malloc(size));
+	while (w < i)
+	{
+		dest[j][w] = src[i];
+		w++;	
+	}
+  	dest[j][w] = 0;
+}
+char	**worker(char *src, char **dest)
+{
+	int i;
+	int j;
+	int w;
+	
+	i = 0;
+  	j = 0;
+  	w = 0;
+  	while (src[i])
+  	{	
+  		if checkspace_backward
+  		{
+  			dest[j] = (char*)malloc(i - w);
+  			writer(dest, i, j, w);
+  			j++;
+  		}
+  	if checkspace_forward
+  		w = i + 1;
+  	i++;
+  	}
+  	return (dest);
 }
 char    **ft_split_whitespaces(char *src)
 {
-	int 	i;
-	int 	word_ct;
-	int 	j;
 	char	**dest;
 	
-	word_ct = ct_words(str);
-	dest = (char*)malloc(word_ct);
-  	i = 0;
-  	j = 0;
-  	word_ct = 0;
-  	while (src[i])
-  	{	
-  		if ((src[i] == ' ') || (src[i] == 9) || (src[i] == '\n'))
-  		{	
-  			while (w <= i)
-  			{
-  			dest[j][w] = src[w]
-  			w++;
-  			}
-  			j++;
-  		}
-  	i++;
-  	}
-  	
-  	
+	dest = (char**)malloc(words_counter(src));
+	return (worker(src, dest));
+}
+  
   	
   	
