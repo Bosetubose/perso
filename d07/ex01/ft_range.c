bc04c144cb5d0b3b6e4d527f5bf46560c9f9d15f
Bosetubose <guillaume.dblms@gmail.com>
2016-08-17 16:24:22 +0200

    Update ft_range.c

--- d07/ex01/ft_range.c
+++ d07/ex01/ft_range.c
@@ -1,13 +1,18 @@
+#define	abs(x)	((x) < 0 ? -(x) : (x))
+
 char 	*ft_range(int min, int max)
 {
 	int	tab;
 	int i;
 	
-	if (min > max)
-		return (0);
-	tab = (int*)malloc(4*(max - min));	
+	if (min < max)
+	{
+	tab = (int*)malloc(4*(max - abs(min)));	
 	i = 0;
 	while (min < max)
 		tab[i++] = min++;
+	}
+	else
+		*tab = NULL;
 	return(tab);
 }
